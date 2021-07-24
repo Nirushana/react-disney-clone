06bcac899beb80d4e5a83f66799d0e7e5833c668
Nirushana <kavinda.n.t@gmail.com>
2021-07-25 00:35:07 +0530

    adding content

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,7 +3,18 @@ import styled from "styled-components";
 const Login = (props) => {
   return (
     <Container>
-      <Content><BgImage /></Content>
+      <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+          <SignUp>GET ALL THERE</SignUp>
+          <Description>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </Description>
+        </CTA>
+        <BgImage />
+      </Content>
     </Container>
   );
 };
@@ -17,30 +28,79 @@ const Container = styled.section`
 `;
 
 const Content = styled.div`
- margin-bottom: 10wv;
- width: 100%;
- position: relative;
- min-height: 100vh;
- box-sizing: border-box;
- display: flex;
- justify-content: center;
- align-items: center;
- flex-direction: column;
- padding: 80px 40px;
- height: 100%;
+  margin-bottom: 10wv;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
 `;
 
 const BgImage = styled.div`
-height: 100%;
-background-position: top;
-background-repeat: no-repeat;
-background-size: cover;
- background-image: url('/images/login-background.jpg');
- position: absolute;
- top: 0;
- right: 0;
- left: 0;
- z-index: -1;
-`
+  height: 100%;
+  background-position: top;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div`
+  margin-bottom: 2vw;
+  max-width: 650px;
+  flex-wrap: wrap;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 0;
+  align-items: center;
+  text-align: center;
+  margin-right: auto;
+  margin-left: auto;
+  transition-timing-function: ease-out;
+  transition: opacity 0.2s;
+  width: 100%;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 500px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+
+const SignUp = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+ color: hsla(0, 0%, 93.5%m 1);
+ font: 12px;
+ margin: 0 0 24px;
+ line-height: 1.5;
+`;
 
 export default Login;
