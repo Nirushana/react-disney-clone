96d465e8566202b95c62e148d76e075a96fb6ec9
Nirushana <kavinda.n.t@gmail.com>
2021-07-24 23:34:27 +0530

    LandingPage and Images

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Login = (props) => {
   return (
     <Container>
-      <Content>Content</Content>
+      <Content><BgImage /></Content>
     </Container>
   );
 };
@@ -25,6 +25,22 @@ const Content = styled.div`
  display: flex;
  justify-content: center;
  align-items: center;
+ flex-direction: column;
+ padding: 80px 40px;
+ height: 100%;
 `;
+
+const BgImage = styled.div`
+height: 100%;
+background-position: top;
+background-repeat: no-repeat;
+background-size: cover;
+ background-image: url('/images/login-background.jpg');
+ position: absolute;
+ top: 0;
+ right: 0;
+ left: 0;
+ z-index: -1;
+`
 
 export default Login;
