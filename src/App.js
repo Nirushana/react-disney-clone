b773e8f85e85aa3594276adaa630a913dd9c5dab
Nirushana <kavinda.n.t@gmail.com>
2021-07-26 03:11:47 +0530

    NavMenu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './Components/Login';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Switch>
           <Route exact path='/'>
             <Login />
