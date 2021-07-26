53c64917217c4f19b9b88b587fa2101d5b3eefa5
Nirushana <kavinda.n.t@gmail.com>
2021-07-26 14:42:10 +0530

    NavBar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,102 +1,132 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 function Header(Props) {
-    return (
-        <Nav>
-            <Logo>
-                <img src='/images/logo.svg' alt='Disney+' />
-            </Logo>
-            <NavMenu>
-                <a href='/home'>
-                    <img src='/images/home-icon.svg' alt='home'/>
-                    <span>HOME</span>
-                </a>
-            </NavMenu>
-        </Nav>
-    )
+  return (
+    <Nav>
+      <Logo>
+        <img src="/images/logo.svg" alt="Disney+" />
+      </Logo>
+      <NavMenu>
+        <a href="/home">
+          <img src="/images/home-icon.svg" alt="home" />
+          <span>HOME</span>
+        </a>
+        <a href="/search">
+          <img src="/images/search-icon.svg" alt="home" />
+          <span>SEARCH</span>
+        </a>
+        <a href="/watchlist">
+          <img src="/images/watchlist-icon.svg" alt="home" />
+          <span>WATCHLIST</span>
+        </a>
+        <a href="/original">
+          <img src="/images/original-icon.svg" alt="home" />
+          <span>ORIGINAL</span>
+        </a>
+        <a href="/movie">
+          <img src="/images/movie-icon.svg" alt="home" />
+          <span>MOVIE</span>
+        </a>
+        <a href="/series">
+          <img src="/images/series-icon.svg" alt="home" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+    </Nav>
+  );
 }
 
 const Nav = styled.div`
- position: fixed;
- top: 0;
- left: 0;
- right: 0;
- height: 70px;
- background-color: #090b13;
- display: flex;
- justify-content: space-between;
- align-items: center;
- padding: 0 36px;
- letter-spacing: 16px;
- z-index: 3;
-`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
 
 const Logo = styled.a`
- padding: 0;
- width: 80px;
- margin-top: 4px;
- max-height: 70px;
- font-size: 0;
- display: inline-block;
-  img{
-      display: block;
-      width: 100%;
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+  img {
+    display: block;
+    width: 100%;
   }
-`
+`;
 const NavMenu = styled.div`
- align-items: center;
- display: flex;
- flex-flow: row nowrap;
- height: 100%;
- justify-content: flex-end;
- margin: 0;
- padding: 0px;
- position: relative;
- margin-right: auto;
- margin-left: 25px;
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0;
+  padding: 0px;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
 
+  @media (max-width: 768px) {
+    display: none;
+  }
 
- @media (max-width: 768px) {
-     display: none;
- }
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
 
- a{
-     display: flex;
-     align-items: center;
-     padding: 0 12px;
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      z-index: auto;
+    }
 
-     img{
-         height: 20px;
-         min-width: 20px;
-         width: 20px;
-         z-index: auto;
-     }
+    span {
+      color: rgba(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 8px;
+      white-space: nowrap;
+      position: relative;
 
-     span{
-         color: rgba(249, 249, 249);
-         font-size: 13px;
-         letter-spacing: 1.42px;
-         line-height: 1.08;
-         padding: 2px 8px;
-         white-space: nowrap;
-         position: relative;
-     }
+      &:before {
+        background-color: rgba(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        left: 0px;
+        opacity: 0;
+        position: absolute;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
 
-     &:before{
-         background-color: rgba(249, 249, 249);
-         border-radius: 0px 0px 4px 4px;
-         bottom: -6px;
-         content: '';
-         opacity: 0;
-         position: absolute;
-         right: 0px;
-         transform-origin: left center;
-         transform: scaleX(0);
-         
-     }
- }
-`
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1;
+      }
+    }
+  }
+`;
 
-
-export default Header
+export default Header;
